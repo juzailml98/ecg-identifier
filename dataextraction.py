014893232e142e2aa8dea6fbcd0b3edc6f9bb1bf
juzailml98 <juzailofficial@gmail.com>
2021-09-23 15:36:04 +0530

    changed dataextraction

--- dataextraction.py
+++ dataextraction.py
@@ -3,9 +3,6 @@ import glob
 import numpy as np
 def record_collector():
     atr_file_path=glob.glob('dataset/*.atr')
-    #print(atr_file_path)
-    #atr_file_path = [path[:-4] for path in atr_file_path]
-    #atr_file_path.sort()
     return atr_file_path
 
 print(record_collector())
